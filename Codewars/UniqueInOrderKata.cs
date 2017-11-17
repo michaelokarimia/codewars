1b4c9c1af55f18e18d1e1280aabd08aff63b4866
Michael Okarimia <git@michaelokarimia.com>
2017-11-17 13:45:50 +0000

    Unique in order tests

---
+++ Codewars/UniqueInOrderKata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    public class UniqueInOrderKata
+    {
+        public static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable)
+        {
+            var results = new List<T>();
+
+
+            //your code here...
+            return results;
+        }
+    }
+}
---
+++ KataTests/UniqueInOrderTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codewars;
+
+namespace KataTests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class UniqueInOrderTests
+    {
+        [Test]
+        public void EmptyTest()
+        {
+            Assert.AreEqual("", UniqueInOrderKata.UniqueInOrder(""));
+        }
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual("ABCDAB", UniqueInOrderKata.UniqueInOrder("AAAABBBCCDAABBB"));
+            Assert.AreEqual("ABBCcAD", UniqueInOrderKata.UniqueInOrder("ABCcAD"));
+
+            Assert.AreEqual("12233", UniqueInOrderKata.UniqueInOrder("123"));
+
+            Assert.AreEqual(new List<double> { 1.1, 2.2, 2.2, 3.3 }, UniqueInOrderKata.UniqueInOrder(new List<double> { 1.1, 2.2, 3.3 }));
+
+
+
+
+        }
+    }
+}
