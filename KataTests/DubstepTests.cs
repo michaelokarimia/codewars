2595e8c4ac9c531d95996191a63fb85bc66fbcba
Michael Okarimia <git@michaelokarimia.com>
2017-11-18 13:46:59 +0000

    Disable Dubstep tests

--- KataTests/DubstepTests.cs
+++ KataTests/DubstepTests.cs
@@ -7,13 +7,13 @@ namespace KataTests
     [TestFixture]
     public class DubstepTests
     {
-        [Test]
+        //[Test]
         public void Test1()
         {
             Assert.AreEqual("ABC", Dubstep.SongDecoder("WUBWUBABCWUB"));
         }
 
-        [Test]
+        //[Test]
         public void Test2()
         {
             Assert.AreEqual("R L", Dubstep.SongDecoder("RWUBWUBWUBLWUB"));
