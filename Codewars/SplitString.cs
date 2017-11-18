db82c1bcfff4b2bbe32d0fd76433f6f099f88276
Michael Okarimia <git@michaelokarimia.com>
2017-11-18 12:56:19 +0000

    SplitString into pairs
    https://www.codewars.com/kata/515de9ae9dcfc28eb6000001

---
+++ Codewars/SplitString.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    public class SplitString
+    {
+        /* Complete the solution so that it splits the string into pairs of two characters. 
+         * If the string contains an odd number of characters 
+         * then it should replace the missing second character of the final pair with an underscore ('_').
+         * 
+         * 
+         * 
+         */
+        public static string[] Solution(string str)
+        {
+            //append an underscore to str if it has odd number of chars
+            if (str.Length % 2 ==1)
+            {
+                str += "_";
+            }
+
+            int pairCount = (str.Length / 2);
+
+            string[] result = new string[pairCount];
+
+            string pair = "";
+
+            int pairIndex = 0;
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                pair += str[i];
+
+                if (pair.Length == 2)
+                {
+                    result[pairIndex] = pair;
+                    pairIndex++;
+                    pair = "";
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ KataTests/SplitStringTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Codewars;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class SplitStringTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new string[] { "ab", "c_" }, SplitString.Solution("abc"));
+            Assert.AreEqual(new string[] { "ab", "cd", "ef" }, SplitString.Solution("abcdef"));
+        }
+    }
+}
