667464f8caeebd7ba736466dc7680d17822d235e
Michael Okarimia <git@michaelokarimia.com>
2017-11-18 18:04:46 +0000

    RGB to Hex converter
    https://www.codewars.com/kata/rgb-to-hex-conversion/csharp

---
+++ Codewars/RGBConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Codewars
+{
+    public class RGBConverter
+    {
+        public static string Rgb(int red, int green, int blue)
+        {
+            return string.Format("{0}{1}{2}", convertToHex(red), convertToHex(green), convertToHex(blue));
+        }
+
+        private static string convertToHex(int value)
+        {
+            //X2 means return two digits and zero pad if needed
+            if (value < 0)
+            {
+                return 0.ToString("X2");
+            }
+            if (value > 255)
+            {
+                return 255.ToString("X2");
+            }
+
+            return value.ToString("X2");
+        }
+    }
+}
---
+++ KataTests/RGBConverterTests.cs
@@ -0,0 +1,21 @@
+﻿using Codewars;
+using NUnit.Framework;
+
+namespace KataTests
+{
+    [TestFixture]
+    public class RGBConverterTests
+    {
+        [Test]
+        public void FixedTests()
+        {
+            Assert.AreEqual("000000", RGBConverter.Rgb(0, 0, 0));
+            Assert.AreEqual("FFFFFF", RGBConverter.Rgb(255, 255, 255));
+            Assert.AreEqual("FFFFFF", RGBConverter.Rgb(255, 255, 300));
+            Assert.AreEqual("9400D3", RGBConverter.Rgb(148, 0, 211));
+            Assert.AreEqual("9400D3", RGBConverter.Rgb(148, -20, 211), "Handle negative numbers.");
+            Assert.AreEqual("90C3D4", RGBConverter.Rgb(144, 195, 212));
+            Assert.AreEqual("D4350C", RGBConverter.Rgb(212, 53, 12), "Consider single hex digit numbers.");
+        }
+    }
+}
