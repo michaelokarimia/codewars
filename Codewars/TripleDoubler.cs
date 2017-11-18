5bd5e4b0d96e8aca56b86a882ab240609641eeac
Michael Okarimia <git@michaelokarimia.com>
2017-11-18 18:35:23 +0000

    Triple trouble solution, works but needs refactor
    https://www.codewars.com/kata/triple-trouble-1/train/csharp

---
+++ Codewars/TripleDoubler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Codewars
+{
+    public class TripleDoubler
+    {
+        public static int TripleDouble(long num1, long num2)
+        {
+            bool hasTripleDouble = false;
+
+            bool isTriple = false;
+
+            bool isDouble = false;
+
+            char charToCheck = '#';
+
+            var first = num1.ToString();
+
+            var second = num2.ToString();
+
+            if(first.Length < 3)
+            { return 0; }
+
+            for(int i = 2; i< first.Length && !isTriple; i++)
+            {
+                if(first[i] == first[i-1] && first[i] == first[i - 2])
+                {
+                    isTriple = true;
+                    charToCheck = first[i];
+                }
+
+            }
+
+            if(isTriple && charToCheck != '#')
+            {
+                //check for double in second number
+
+                if(second.Length < 2)
+                {
+                    return 0;
+                }
+
+                for(int i = 1; i < second.Length && !isDouble; i++)
+                {
+                    if(second[i] == charToCheck && second[i] == second[i-1])
+                    {
+                        isDouble = true;
+                        return 1;
+                    }
+                }
+            }
+
+            return 0;
+        }
+    }
+}
