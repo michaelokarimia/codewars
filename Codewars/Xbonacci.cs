cbffed9f308fb9729acca6e72ba4c6ff1cd6198c
Michael Okarimia <git@michaelokarimia.com>
2017-11-18 15:47:35 +0000

    XBonacci after reading the instructions properly

---
+++ Codewars/Xbonacci.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars
+{
+    public class Xbonacci
+    {
+        public double[] Tribonacci(double[] signature, int n)
+        {
+            // if n==0, then return an array of length 1, containing only a 0
+
+            if (n == 0)
+                return new double[1] { 0};
+
+
+            var sequence = new List<Double>();
+
+            sequence.AddRange(signature);
+
+            var timesToRepeat = n;
+
+            while (timesToRepeat > (signature.Length))
+
+            {
+                var summedValue = sequence[sequence.Count - 1] + sequence[sequence.Count - 2] + sequence[sequence.Count - 3];
+
+                sequence.Add(summedValue);
+
+                timesToRepeat--;
+            }
+
+            var tempList = sequence.Take(n).ToArray();
+
+
+            return tempList;
+        }
+    }
+}
---
+++ KataTests/XbonacciTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using Codewars;
+
+namespace KataTests
+{
+    class XbonacciTests
+    {
+        private Xbonacci variabonacci;
+
+        [SetUp]
+        public void SetUp()
+        {
+            variabonacci = new Xbonacci();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            variabonacci = null;
+        }
+
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual(new double[] { 1, 1, 1, 3, 5, 9, 17, 31, 57, 105 }, variabonacci.Tribonacci(new double[] { 1, 1, 1 }, 10));
+            Assert.AreEqual(new double[] { 0, 0, 1, 1, 2, 4, 7, 13, 24, 44 }, variabonacci.Tribonacci(new double[] { 0, 0, 1 }, 10));
+            Assert.AreEqual(new double[] { 0, 1, 1, 2, 4, 7, 13, 24, 44, 81 }, variabonacci.Tribonacci(new double[] { 0, 1, 1 }, 10));
+
+            Assert.AreEqual(new double[] { 0 }, variabonacci.Tribonacci(new double[] { 1, 2, 4 }, 0));
+
+            
+
+            Assert.AreEqual(new double[] { 1, 0, 0, 1, 1, 2, 4, 7, 13, 24  }, variabonacci.Tribonacci(new double[] { 1, 0, 0 }, 10));
+
+            Assert.AreEqual(new double[] { 0, 1, 0, 1, 2, 3, 6, 11, 20, 37 }, variabonacci.Tribonacci(new double[] { 0, 1, 0 }, 10));
+
+            Assert.AreEqual(new double[] { 0, 0, 1, 1, 2, 4, 7, 13, 24, 44 }, variabonacci.Tribonacci(new double[] { 0, 0, 1 }, 10));
+
+            Assert.AreEqual(new double[] { 1, 0, }, variabonacci.Tribonacci(new double[] { 1, 0, 0 }, 2));
+
+
+            Assert.AreEqual(new double[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, variabonacci.Tribonacci(new double[] { 0, 0, 0 }, 10));
+
+
+
+
+        }
+    }
+}
