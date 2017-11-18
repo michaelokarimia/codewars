27f46231d12e6efb6b4aa9bb61406fe987cb3209
Michael Okarimia <git@michaelokarimia.com>
2017-11-18 13:40:22 +0000

    dubstep kata WUBWUBWUB
    https://www.codewars.com/kata/dubstep/csharp

---
+++ Codewars/Dubstep.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Codewars
+{
+    public class Dubstep
+    {
+        public static string SongDecoder(string input)
+        {
+            string result = "";
+
+            string[] searchStr = { "WUB"};
+
+            var tempResult = input.Split(searchStr, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(string s in tempResult)
+            {
+                result += " " + s;
+            }
+
+            return result.Trim();
+        }
+    }
+}
---
+++ KataTests/DubstepTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Codewars;
+
+namespace KataTests
+{
+
+    [TestFixture]
+    public class DubstepTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("ABC", Dubstep.SongDecoder("WUBWUBABCWUB"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual("R L", Dubstep.SongDecoder("RWUBWUBWUBLWUB"));
+        }
+
+    }
+}
