c56cc7c76a14bce0fb19dc091a5fed584e8ce785
Michael Okarimia <git@michaelokarimia.com>
2017-11-18 14:05:02 +0000

    Duplicate char counter
    https://www.codewars.com/kata/counting-duplicates/csharp

---
+++ Codewars/DuplicateCounter.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Codewars
+{
+    public class DuplicateCounter
+    {
+        /*
+         * Write a function that will return the count of distinct case-insensitive alphabetic characters
+         * and numeric digits that occur more than once in the input string. 
+         * The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits
+         * 
+         * */
+
+
+        public static int DuplicateCount(string str)
+        {
+
+            var duplicateCount = 0;
+
+            var dict = new Dictionary<char, bool>();
+
+            //iterate through each char in the string
+            foreach(char c in str.ToLowerInvariant())
+            {
+                //if char already exists in the string, 
+                if(dict.ContainsKey(c))
+                {
+                    var alreadyCountedAsDuplicate = dict[c];
+                    
+                    if(!alreadyCountedAsDuplicate)
+                    {
+                        duplicateCount++;
+                        dict[c] = true;
+                    }
+                    
+                }
+                else
+                {
+                    //else it's new, so add it to the dict
+
+                    dict.Add(c, false);
+                }
+
+            }
+
+
+
+            return duplicateCount;
+        }
+    }
+}
---
+++ KataTests/DuplicateCountTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Codewars;
+
+namespace KataTests
+{
+    using System;
+    using System.Text.RegularExpressions;
+    using System.Collections.Generic;
+    using System.Linq;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DuplicateCountTest
+    {
+        [Test]
+        public void DuplicateCountTests()
+        {
+            Assert.AreEqual(0, DuplicateCounter.DuplicateCount(""));
+            Assert.AreEqual(0, DuplicateCounter.DuplicateCount("abcde"));
+            Assert.AreEqual(2, DuplicateCounter.DuplicateCount("aabbcde"));
+            Assert.AreEqual(2, DuplicateCounter.DuplicateCount("aabBcde"), "should ignore case");
+            Assert.AreEqual(1, DuplicateCounter.DuplicateCount("Indivisibility"));
+            Assert.AreEqual(2, DuplicateCounter.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+        }
+    }
+}
